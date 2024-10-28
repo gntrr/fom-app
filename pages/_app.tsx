e86ab74beb990fbdf13f3ffdb73f9ace47aaac55
gntrr <wima2002@gmail.com>
2024-10-28 13:42:35 +0700

    add Vercel Speed Insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../styles/theme';
 import type { AppProps } from 'next/app';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <SpeedInsights />
     </ChakraProvider>
   );
 }
